ec743856bf8f82d94ccc89ab2041c247d5554924
Mono-Ling <zouzongfeng070812@163.com>
2026-07-04 12:20:07 +0800

    导入TMP，修复氧气量进度条

--- Assets/Scripts/UI/OxygenGauge.cs
+++ Assets/Scripts/UI/OxygenGauge.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class OxygenGauge : MonoBehaviour
+public class OxygenGauge : BaseUI
 {
     public Image _gaugeImage;
+    public TextMeshProUGUI _oxygenText;
     private int _currentOxygen = 0;
     private int _maxOxygen = 0;
-    public void Start()
+    protected override void OnInit()
     {
+        if(_gaugeImage == null)
+            Debug.LogError("【空引用】GaugeImage is null");
+        if(_oxygenText == null)
+            Debug.LogError("【空引用】OxygenText is null");
         EventBus.Instance.AddListener<int>(EventType.OnOxygenChange, GetCurrentOxygen);
         EventBus.Instance.AddListener<int>(EventType.OnMaxOxygenChange, GetMaxOxygen);
     }
@@ -18,12 +24,14 @@ public class OxygenGauge : MonoBehaviour
         _currentOxygen = value;
         float fillAmount = (float)_currentOxygen / Mathf.Clamp01(_maxOxygen);
         _gaugeImage.fillAmount = fillAmount;
+        _oxygenText.text = $"{_currentOxygen}/{_maxOxygen}";
     }
     public void GetMaxOxygen(int value)
     {
         _maxOxygen = value;
         float fillAmount = (float)_currentOxygen / Mathf.Clamp01(_maxOxygen);
         _gaugeImage.fillAmount = fillAmount;
+        _oxygenText.text = $"{_currentOxygen}/{_maxOxygen}";
     }
     void OnDestroy()
     {
